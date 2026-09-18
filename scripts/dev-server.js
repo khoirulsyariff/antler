759966b38e7efa0cf7139b6c5411288b47c84fcb
Khoirul Syarifudin <khoirulsyarif@syarifxyz-3.local>
2026-09-18 10:18:42 +0700

    Configure as pure static site and move dev server to scripts

--- scripts/dev-server.js
+++ scripts/dev-server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const PORT = 3000;
-const BASE_DIR = __dirname;
+const BASE_DIR = path.join(__dirname, '..');
 
 const MIME_TYPES = {
   '.html': 'text/html; charset=utf-8',
